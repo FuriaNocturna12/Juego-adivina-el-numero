0672e35db211c593b44a3f6626d3abe5ecb4ed96
Luis Aguila <luis.aguilacampos@gmail.com>
2025-07-30 02:20:58 -0500

    Mi primer juego en Github

---
+++ app.js
@@ -0,0 +1,43 @@
+//Variables 
+
+ /*
+let numeroSecreto = 4;
+let numeroUsuario = 0;
+let intentos = 1;
+let palabraVeces = "vez";
+while (numeroUsuario != numeroSecreto) {
+    numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
+
+    console.log(numeroUsuario);
+
+    //Este código realiza 
+    //la comparación
+
+    if(numeroUsuario == numeroSecreto) 
+    //Alerta de acierto
+        {alert (`Acertaste, el numero es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}.`);
+
+    } else {
+        if (numeroUsuario > numeroSecreto) {
+            alert("El numero secreto es menor");
+            } else {
+            alert("El número secreto es mayor");
+        }
+    //Incrementamos el contador cuando no acierta
+        intentos = intentos + 1;
+        palabraVeces = "veces";
+    //Alerta de no acierto
+        alert("Lo siento, no acertaste");
+    }
+}
+   
+*/
+
+let contador = 1;
+console.log("Contador ascendente del 1 al 10:");
+while(contador <=10); {
+    console.log(contador);
+    contador++; //Incrementa el contador en 1
+
+}
+
